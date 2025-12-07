fce686d7d0121c7bdce163a6441c2190259802ee
Emmanuel Eyung <eeyung@noreply.acad.learn2earn.ng>
2025-12-07 01:07:34 +0000

    Update quadchecker/main.go

--- quadchecker/main.go
+++ quadchecker/main.go
@@ -4,69 +4,40 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"sort"
+	"strconv"
 	"strings"
 )
 
-func genQuad(w, h int, topLeft, topRight, bottomLeft, bottomRight, edge, middle string) string {
-	if w <= 0 || h <= 0 {
-		return ""
-	}
-	lines := make([]string, h)
-	for r := 0; r < h; r++ {
-		switch r {
-		case 0:
-			if w == 1 {
-				lines[r] = topLeft
-			} else {
-				lines[r] = topLeft + strings.Repeat(edge, w-2) + topRight
-			}
-		case h - 1:
-			if w == 1 {
-				lines[r] = bottomLeft
-			} else {
-				lines[r] = bottomLeft + strings.Repeat(edge, w-2) + bottomRight
-			}
-		default:
-			if w == 1 {
-				lines[r] = middle
-			} else {
-				lines[r] = middle + strings.Repeat(" ", w-2) + middle
-			}
-		}
-	}
-	return strings.Join(lines, "\n")
-}
-
 func main() {
-	raw, _ := io.ReadAll(os.Stdin)
-	input := strings.TrimRight(string(raw), "\n")
-	if input == "" {
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
 		fmt.Println("Not a quad function")
 		return
 	}
+	inputStr := strings.TrimRight(string(input), "\n")
 
-	lines := strings.Split(input, "\n")
-	w, h := len(lines[0]), len(lines)
-	for _, L := range lines {
-		if len(L) != w {
-			fmt.Println("Not a quad function")
-			return
-		}
-	}
+	quads := []string{"quadA", "quadB", "quadC", "quadD", "quadE"}
 
-	quads := map[string][6]string{
-		"quadA": {"o", "o", "o", "o", "-", "|"},
-		"quadB": {"/", "\\", "\\", "/", "*", "*"},
-		"quadC": {"A", "A", "C", "C", "B", "B"},
-		"quadD": {"A", "C", "A", "C", "B", "B"},
-		"quadE": {"A", "C", "C", "A", "B", "B"},
-	}
+	var matches []string
 
-	matches := []string{}
-	for name, chars := range quads {
-		if input == genQuad(w, h, chars[0], chars[1], chars[2], chars[3], chars[4], chars[5]) {
-			matches = append(matches, name)
+	for _, quad := range quads {
+		for x := 1; x <= 50; x++ {
+			for y := 1; y <= 50; y++ {
+				cmd := exec.Command("./"+quad, strconv.Itoa(x), strconv.Itoa(y))
+				output, err := cmd.Output()
+
+				if err != nil {
+					continue
+				}
+
+				outStr := strings.TrimRight(string(output), "\n")
+
+				if outStr == inputStr {
+					matches = append(matches, fmt.Sprintf("[%s] [%d] [%d]", quad, x, y))
+				}
+			}
 		}
 	}
 
@@ -76,9 +47,5 @@ func main() {
 	}
 
 	sort.Strings(matches)
-	out := []string{}
-	for _, m := range matches {
-		out = append(out, fmt.Sprintf("[%s] [%d] [%d]", m, w, h))
-	}
-	fmt.Println(strings.Join(out, " || "))
+	fmt.Println(strings.Join(matches, " || "))
 }

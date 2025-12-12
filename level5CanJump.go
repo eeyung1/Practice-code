1d0ee3caa68fa224f9f94ad36103640f808732f0
Emmanuel Eyung <eeyung@noreply.acad.learn2earn.ng>
2025-12-12 01:28:47 +0000

    Update level5CanJump.go

--- level5CanJump.go
+++ level5CanJump.go
@@ -2,19 +2,17 @@ package piscine
 
 func CanJump(nums []uint) bool {
     if len(nums) == 0 {
-        return false // No elements in the array, can't jump to the last index
+        return false 
     }
     
-    maxReach := 0 // This will track the furthest position we can reach
+    maxReach := 0 
     
     for i := 0; i < len(nums); i++ {
         if i > maxReach {
-            return false // If we reach an index that is beyond the maximum reachable index, return false
+            return false 
         }
-        // Update maxReach to the maximum of current position + steps we can take at current position
         maxReach = max(maxReach, i+int(nums[i]))
         
-        // If we can already reach the last index, no need to continue
         if maxReach >= len(nums)-1 {
             return true
         }
@@ -22,7 +20,6 @@ func CanJump(nums []uint) bool {
     return false
 }
 
-// Helper function to get the maximum of two values
 func max(a, b int) int {
     if a > b {
         return a

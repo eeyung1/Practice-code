5da67cda8012e9d28629bf2663367e46901bdd6b
eeyung <eyungemmanuel@gmail.com>
2026-03-06 14:28:39 +0000

    Rest API

--- restapi/main.go
+++ restapi/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -10,11 +11,11 @@ func main() {
 		fmt.Fprintln(w, "Hello World!")
 	})
 
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "This is my first Go server!")
 	})
 
-	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
 		name := r.URL.Query().Get("name")
 		if name == "" {
 			fmt.Fprintln(w, "Hello stranger!")
@@ -22,6 +23,22 @@ func main() {
 			fmt.Fprintf(w, "Hello %s!", name)
 		}
 	})
+
+type User struct {
+    Name string `json:"name"`
+    City string `json:"city"`
+    Age  int    `json:"age"`
+}
+
+http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    user := User{
+        Name: "Alice",
+        City: "Lagos",
+        Age:  25,
+    }
+    json.NewEncoder(w).Encode(user)
+})
 
 	fmt.Println("Server running on port 8080...")
 	http.ListenAndServe(":8080", nil)

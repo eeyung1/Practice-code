c69b946748d9403c0fc47937453b4877a1c6c6d6
eeyung <eyungemmanuel@gmail.com>
2026-03-06 18:04:44 +0000

    Rest API

--- restapi/main.go
+++ restapi/main.go
@@ -6,6 +6,13 @@ import (
 	"net/http"
 )
 
+type User struct {
+    Name string `json:"name"`
+    City string `json:"city"`
+    Age  int    `json:"age"`
+}
+
+
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World!")
@@ -24,11 +31,16 @@ func main() {
 		}
 	})
 
-type User struct {
-    Name string `json:"name"`
-    City string `json:"city"`
-    Age  int    `json:"age"`
-}
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    users := []User{
+        {Name: "Alice", City: "Lagos", Age: 25},
+        {Name: "Bob", City: "Abuja", Age: 30},
+        {Name: "Charlie", City: "Ibadan", Age: 22},
+    }
+    json.NewEncoder(w).Encode(users)
+	})
+
 
 http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
@@ -43,3 +55,6 @@ http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Server running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
+
+
+//curl http://localhost:8080/users

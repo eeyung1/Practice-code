3bf6021c9bc2f74ba5880f0cfa77d26113a2e1d3
eeyung <eyungemmanuel@gmail.com>
2026-03-10 09:23:05 +0000

    text editor function

--- builder/editor.go
+++ builder/editor.go
@@ -52,8 +52,54 @@ func applyBin(text string) string {
 	return result
 }
 
+func applyUp(text string) string {
+	worded := strings.Fields(text)
+
+	for i := 0; i < len(worded); i++ {
+		if worded[i] == "(up)" {
+			worded[i-1] = strings.ToUpper(worded[i-1])
+			worded = append(worded[:i], worded[i+1:]...)
+			i--
+		}
+	}
+	result := strings.Join(worded, " ")
+	return result
+}
+
+func applyLow(text string) string {
+	worded := strings.Fields(text)
+
+	for i := 0; i < len(worded); i++ {
+		if worded[i] == "(low)" {
+		worded[i-1] = strings.ToLower(worded[i-1])
+		worded = append(worded[:i], worded[i+1:]...)
+		i--
+		}
+	}
+
+	result := strings.Join(worded, " ")
+	return result
+}
+
+func applyCap(text string) string {
+	worded := strings.Fields(text)
+
+	for i := 0; i < len(worded); i++ {
+		if worded[i] == "(cap)" {
+			worded[i-1] = strings.ToUpper(string(worded[i-1][0])) + worded[i-1][1:]
+			worded = append(worded[:i], worded[i+1:]...)
+		}
+	}
+
+	result := strings.Join(worded, " ")
+	return result
+}
+
 func editor(text string) string {
 	text = applyHex(text)
 	text = applyBin(text)
+	text = applyUp(text)
+	text = applyLow(text)
+	text = applyCap(text)
 	return text
 }

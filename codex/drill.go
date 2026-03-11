e6b6815443e0112b77c30018b39421bced9d632a
eeyung <eyungemmanuel@gmail.com>
2026-03-11 08:20:11 +0000

    multiple return statements

--- codex/drill.go
+++ codex/drill.go
@@ -1,36 +1,58 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"strconv"
+// 	"strings"
+// )
+
+// func BaseConv(num string) (int64, error, string) {
+// 	word := strings.Fields(num)
+// 	for i := 0; i < len(word); i++ {
+// 		if word[i] == "(hex)" {
+// 			data, err := strconv.ParseInt(word[i-1], 16, 64)
+// 			if err != nil {
+// 				return 0, err, "logic"
+// 			}
+
+// 			return data, nil, "logic"
+
+// 		} else if word[i] == "(bin)" {
+// 			data, err := strconv.ParseInt(word[i-1], 2, 64)
+// 			if err != nil {
+// 				return 0, err, "logic"
+// 			}
+
+// 			return data, nil, "logic"
+// 		}
+// 	}
+
+// 	return 0, nil, ""
+// }
+
+// func main() {
+// 	fmt.Println(BaseConv("FF (hex)"))
+// 	fmt.Println(BaseConv("101 (bin)"))
+// }
+
 package main
 
 import (
 	"fmt"
 	"strconv"
-	"strings"
 )
 
-func BaseConv(num string) (int64, error, string) {
-	word := strings.Fields(num)
-	for i := 0; i < len(word); i++ {
-		if word[i] == "(hex)" {
-			data, err := strconv.ParseInt(word[i-1], 16, 64)
-			if err != nil {
-				return 0, err, "logic"
-			}
-
-			return data, nil, "logic"
-
-		} else if word[i] == "(bin)" {
-			data, err := strconv.ParseInt(word[i-1], 2, 64)
-			if err != nil {
-				return 0, err, "logic"
-			}
-
-			return data, nil, "logic"
-		}
+func hexToDecimal(dec string) (string, error) {
+	data, err := strconv.ParseInt(dec, 16, 64)
+	if err != nil {
+		return "", err
 	}
 
-	return 0, nil, ""
+	result := fmt.Sprintf("%d", data)
+	return result, nil
 }
 
-func main() {
-	fmt.Println(BaseConv("FF (hex)"))
-	fmt.Println(BaseConv("101 (bin)"))
+func main(){
+	fmt.Println(hexToDecimal("1E"))
+	fmt.Println(hexToDecimal("FF"))
 }

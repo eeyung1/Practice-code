dee8ef7af2194bf8326cfc9aeadec01f95610b74
eeyung <eyungemmanuel@gmail.com>
2026-03-25 07:49:20 +0100

    ascii-art project

---
+++ ascii-art/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"ascii-art/ascii"
+)
+
+func main() {
+	// go run . "Hello" => os.Args = ["./main", "Hello"]
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run . [STRING]")
+		os.Exit(1)
+	}
+
+	input := os.Args[1]
+
+	// Default banner is "standard"
+	result, err := ascii.Render(input, "standard")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Print(result)
+}

0ae5524ccfbbbceb2e69615c519a7a233e8d8ee3
eeyung <eyungemmanuel@gmail.com>
2026-03-25 13:50:55 +0100

    goReloadedExercise

---
+++ goReloadedExercise/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main(){
+	var word string
+	var operation string
+
+	fmt.Println("Input a word:")
+	fmt.Scanln(&word)
+
+	fmt.Println("Input an operation to be carried out: either lastletter, capitalize or deleteIndex: ")
+	fmt.Scanln(&operation)
+
+	if operation == "lastletter" {
+		fmt.Println("The last letter of the word is: ", word[len(word)-1:])
+	} else if operation == "capitalize" {
+		fmt.Println("The capitalize word is: ", strings.ToUpper(word) )
+	}
+
+	if operation == "deleteIndex" {
+		var index int
+		length := len(word)
+
+		for {
+			fmt.Println("Input an index: ")
+			fmt.Scanln(&index)
+
+			if index < 1 || index > length {
+				fmt.Println("Index out of range; Input a valid index")
+				continue
+			}
+
+			result := word[:index] + word[index + 1:]
+			fmt.Println(result)
+			break
+		}
+	}
+
+}

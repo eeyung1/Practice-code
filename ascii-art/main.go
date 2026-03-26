127bbe140de314f67c3c35c34c0e72fdd560a3e8
eeyung <eyungemmanuel@gmail.com>
2026-03-26 17:06:29 +0100

    ascii art

--- ascii-art/main.go
+++ ascii-art/main.go
@@ -9,15 +9,16 @@ import (
 
 func main() {
 	// go run . "Hello" => os.Args = ["./main", "Hello"]
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run . [STRING]")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . [STRING] [banners]")
 		os.Exit(1)
 	}
 
 	input := os.Args[1]
+	banners := os.Args[2]
 
 	// Default banner is "standard"
-	result, err := ascii.Render(input, "standard")
+	result, err := ascii.Render(input, banners)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)

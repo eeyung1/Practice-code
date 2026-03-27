1fd4ee29b2df00837057d74ef70ce539119f0404
eeyung <eyungemmanuel@gmail.com>
2026-03-27 20:54:28 +0100

    CLI_CALCULATOR

--- CLI_CALCULATOR/main.go
+++ CLI_CALCULATOR/main.go
@@ -140,7 +140,7 @@ func main(){
 		}
 
 		if op != 1 && op != 2 && op != 3 && op != 4 && op != 5 && op != 6 {
-			fmt.Println("Select 6 for help guidelines")
+			fmt.Println("Select 5 for help guidelines")
 			fmt.Println()
 			goto start
 		}

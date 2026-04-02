c6d8fab964460bfbcaec7f709b9f8a948c211397
Emmanuel Eyung <eeyung@noreply.acad.learn2earn.ng>
2026-04-02 16:38:46 +0000

    Update codex/type_struct.go

--- codex/type_struct.go
+++ codex/type_struct.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import (
@@ -21,3 +22,48 @@ func main() {
 	fmt.Println("Age: ", pers1.age)
 	fmt.Println("salary: ", pers1.salary)
 }
+*/
+
+/*
+
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	name   string
+	age    int
+	salary int
+}
+
+type men struct {
+	name   string
+	height float64
+	state  string
+}
+
+func main() {
+	var pers1 person
+	var joy men
+
+	joy.name = "Jonathan"
+	joy.height = 1.6
+	joy.state = "Benue"
+
+	pers1.name = "John"
+	pers1.age = 34
+	pers1.salary = 5465654
+
+	fmt.Println("Name: ", joy.name)
+	fmt.Println("Height: ", joy.height)
+	fmt.Println("State: ", joy.state)
+
+	fmt.Println("Name: ", pers1.name)
+	fmt.Println("Age: ", pers1.age)
+	fmt.Println("salary: ", pers1.salary)
+}
+
+*/
+

4e29cde7d5583f32effd0a6e9bbe6483b5d2c607
Emmanuel Eyung <eeyung@noreply.acad.learn2earn.ng>
2026-04-03 08:31:27 +0000

    Add codex/map_manipulation.go

---
+++ codex/map_manipulation.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Directory struct {
+	age   int
+	grade string
+	city  string
+	//school string
+}
+
+func main() {
+	students := map[string]Directory{
+		"Joe":     {age: 15, grade: "S1", city: "Ibadan"},
+		"Chaaba":  {age: 17, grade: "S3", city: "Saki"},
+		"Mariamu": {age: 12, grade: "J2", city: "Kano"},
+		"Chichi":  {age: 16, grade: "S2", city: "Owerri"},
+		"Sean":    {age: 17, grade: "S3", city: "Otukpo"},
+	}
+	students["Musa"] = Directory{age: 14, grade: "S1", city: "Kaduna"}
+
+	students["John"] = Directory{age: 17, grade: "S3", city: "Benue"}
+	students["Jonathan"] = Directory{age: 20, grade: "S1", city: "Taraba"}
+
+	// chichi := students["Chichi"]
+	// chichi.grade = "S3"
+	// students["Chichi"] = chichi
+
+	// delete(students, "Sean")
+
+	// value, exists := students["John"]
+	// if exists {
+	// 	fmt.Println("Found:", value)
+	// } else {
+	// 	fmt.Println("Not Found")
+	// }
+	for key, value := range students {
+		fmt.Println(key, "is", value.age, "years old, in", value.grade, "and lives in", value.city)
+
+		// fmt.Print(students["Joe"])
+	}
+}

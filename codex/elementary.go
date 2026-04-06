e9a3effd47f882001652020d1d2d82f0b50b2ba8
eeyung <eyungemmanuel@gmail.com>
2026-04-06 08:37:35 +0100

    elementary programming

---
+++ codex/elementary.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	
+}
+
+/*
+func main(){
+	make([]int, n)
+	v := make([]int, 3, 6)	
+
+	b := make([]int, 0, 3)
+
+	b = append(b, 7)
+	b = append(b, 6)
+	b = append(b, 5)
+	b = append(b, 9)
+	fmt.Println(b)
+	fmt.Println(v)
+}
+*/
+
+
+
+//slice are windows into an array
+
+// func main() {
+// 	a := []int{1, 2, 3, 4, 5}
+// 	b := a[1:4]
+
+// 	b[0] = 99
+// 	fmt.Println(a)
+// }

9779f0da74ac01e61680b82e7baad83a90deaf23
eeyung <eyungemmanuel@gmail.com>
2026-04-08 20:46:24 +0100

    mappingLogic

--- codex/mappingLogic4/main.go
+++ codex/mappingLogic4/main.go
@@ -17,11 +17,16 @@ func main(){
 	fmt.Println(getCharLines(lines, 0))
 	fmt.Println(getCharLines(lines, 1))
 	fmt.Println(getCharLines(lines, 2))
+	fmt.Println(getCharLines(lines, 10))
 }
 
 func getCharLines(lines []string, index int) []string {
 	start := index*4 + 1
 	end := start + 3
+
+	if end > len(lines) {
+		return []string{}
+	}
 
 	return lines[start:end]
 }

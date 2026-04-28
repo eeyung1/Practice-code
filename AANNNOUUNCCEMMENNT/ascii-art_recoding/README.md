67b4d8206068600b9590ff17d9fd86176f713c6a
eeyung <eyungemmanuel@gmail.com>
2026-04-28 09:48:27 +0100

    loadbanner_complete_function

---
+++ AANNNOUUNCCEMMENNT/ascii-art_recoding/README.md
@@ -0,0 +1,10 @@
+LOADBANNER FUNCTION ALGORITHM
+
+Open File & Error Handle
+Check for Empty File
+Normalize Line Endings (\r\n to \n)
+Split into Lines
+Initialize Map
+Loop with Step 9
+Verify Count (95)
+Final Return
--- AANNNOUUNCCEMMENNT/ascii-art_recoding/banner.go
+++ AANNNOUUNCCEMMENNT/ascii-art_recoding/banner.go
@@ -3,20 +3,47 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	//"strings"
 )
 
 func LoadBanner(filename string) (map[rune][]string, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, fmt.Errorf("Error: file doesn't exist")
+		return nil, fmt.Errorf("error: file doesn't exist")
 	}
 
 	if len(data) == 0 {
 		return nil, fmt.Errorf("file is empty")
 	}
 
-	return nil, nil
+	characters := strings.ReplaceAll(string(data), "\r\n", "\n")
+
+	lines := strings.Split(characters, "\n")
+
+	banner := make(map[rune][]string)
+
+	currentChar := rune(32)
+
+	for i := 1; i < len(lines); i += 9 {
+		if i+8 > len(lines) {
+			break
+		}
+
+		charArt := lines[i : i+8]
+
+		banner[currentChar] = charArt
+
+		currentChar++
+	}
+
+	if len(banner) != 95 {
+		return nil, fmt.Errorf("expected 95 characters, got %d", len(banner))
+	}
+
+
+
+	return banner, nil
 }
 
 

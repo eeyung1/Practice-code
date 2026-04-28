52ea0360657941be9883830100991553e89bcc4d
eeyung <eyungemmanuel@gmail.com>
2026-04-28 22:02:18 +0100

    ascii_art_full_project

--- AANNNOUUNCCEMMENNT/ascii-art_recoding/render.go
+++ AANNNOUUNCCEMMENNT/ascii-art_recoding/render.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	// "fmt"
 	"strings"
 )
 
 func RenderLine(input string, banner map[rune][]string) []string {
 	output := make([]string, 8)
 
-	if input == "" {
-		return output
-	}
-
-	for i := 0; i < 8; i++ {
+	for i := range 8 {
 		var builder strings.Builder
 
 		for _, ch := range input {
@@ -32,61 +27,3 @@ func RenderLine(input string, banner map[rune][]string) []string {
 }
 
 
-// func RenderLine(input string, banner map[rune][]string) []string {
-// 	output := make([]string, 8)
-
-// 	for _, ch := range input {
-// 		art, ok := banner[ch]
-// 		if !ok {
-// 			art = banner[' ']
-// 		}
-
-// 		if art == nil {
-// 			continue
-// 		}
-
-// 		for i := range 8 {
-// 			output[i] += art[i]
-// 		}
-// 	}
-
-// 	return output
-
-// }
-
-// func Renderlines(input string, banner map[rune][]string) []string {
-// 	output := make([]string, 8)
-
-// 	for _, ch := range input {
-// 		art, ok := banner[ch]
-
-// 		if !ok {
-// 			art = banner[' ']
-// 		}
-
-// 		if art == nil {
-// 			continue
-// 		}
-
-// 		for i := range 8 {
-// 			output[i] += art[i]
-// 		}
-// 	}
-
-// 	return output
-// }
-
-// func Renderlines(input string, banner map[rune][]string) []string {
-// 	output := make([]string, 8)
-
-// 	for _, ch := range input {
-// 		//art := banner[ch]
-
-// 		for i := range 8 {
-// 			// output[i] += art[i]
-// 			output[i] += banner[ch][i]
-// 		}
-// 	}
-
-// 	return output
-// }

b570d158d6a1b2b731077f4a9dc358d822c75a35
eeyung <eyungemmanuel@gmail.com>
2026-04-28 21:34:48 +0100

    ascii_art_full_project

--- AANNNOUUNCCEMMENNT/ascii-art_recoding/render.go
+++ AANNNOUUNCCEMMENNT/ascii-art_recoding/render.go
@@ -1,31 +1,58 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"strings"
-// )
+import (
+	// "fmt"
+	"strings"
+)
 
 func RenderLine(input string, banner map[rune][]string) []string {
 	output := make([]string, 8)
 
-	for _, ch := range input {
-		art, ok := banner[ch]
-		if !ok {
-			art = banner[' ']
+	if input == "" {
+		return output
+	}
+
+	for i := 0; i < 8; i++ {
+		var builder strings.Builder
+
+		for _, ch := range input {
+			art, ok := banner[ch]
+
+			if !ok {
+				art = banner[' ']
+			}
+
+			builder.WriteString(art[i])
 		}
 
-		if art == nil {
-			continue
-		}
-
-		for i := range 8 {
-			output[i] += art[i]
-		}
+		output[i] = builder.String()
 	}
 
 	return output
-
 }
+
+
+// func RenderLine(input string, banner map[rune][]string) []string {
+// 	output := make([]string, 8)
+
+// 	for _, ch := range input {
+// 		art, ok := banner[ch]
+// 		if !ok {
+// 			art = banner[' ']
+// 		}
+
+// 		if art == nil {
+// 			continue
+// 		}
+
+// 		for i := range 8 {
+// 			output[i] += art[i]
+// 		}
+// 	}
+
+// 	return output
+
+// }
 
 // func Renderlines(input string, banner map[rune][]string) []string {
 // 	output := make([]string, 8)

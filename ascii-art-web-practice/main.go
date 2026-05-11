74a1c9b2e68fc2b5af20abe708f237d303ccaa0d
eeyung <eyungemmanuel@gmail.com>
2026-05-11 08:57:09 +0100

    update_to_ascii_art_web

--- ascii-art-web-practice/main.go
+++ ascii-art-web-practice/main.go
@@ -7,12 +7,32 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello! You requested the path: %s", r.URL.Path)
+	if r.URL.Path != "/" {
+		http.Error(w, "404 - Page Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "400 - Bad Request: method not allowed", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Home page. Method: %s", r.Method)
+}
+
+func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "400 - Bad Request: method not allowed", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "ASCII art page. Method: %s", asciiArtHandler)
 }
 
 func main(){
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/ascii-art", asciiArtHandler)
 
-	fmt.Println("server running at http://localhost:8080")
+	fmt.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

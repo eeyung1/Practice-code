19b0220d8539bdd35cf4dc0b39f93308615418fc
Emmanuel Eyung <7252+eeyung@noreply.acad.learn2earn.ng>
2026-05-16 19:47:36 +0000

    Update AANNNOUUNCCEMMENNT/ascii-art_recoding/pad_art_rows.go

--- AANNNOUUNCCEMMENNT/ascii-art_recoding/pad_art_rows.go
+++ AANNNOUUNCCEMMENNT/ascii-art_recoding/pad_art_rows.go
@@ -1,25 +1,16 @@
 package main
 
-import (
-	"strings"
-)
+import "strings"
 
-func PadArtRows(rows []string, width int) []string {
+func PadArtRow(rows []string, width int) []string {
 	result := make([]string, len(rows))
 
-	for i, v := range rows {
-		if width <= 0 {
-			result[i] = v
-		}
-
-		padding := width - len(v)
-
-		if padding > 0 {
-			result[i] = v + strings.Repeat(" ", padding)
+	for i, row := range rows {
+		if width > len(row) {
+			result[i] = row + strings.Repeat(" ", width-len(row))
 		} else {
-			result[i] = v
+			result[i] = row
 		}
 	}
-
 	return result
 }

4ebb714ede188c9bf6c2935416548f368c5d455f
eeyung <eyungemmanuel@gmail.com>
2026-05-19 09:24:25 +0100

    understanding_elementary_programming

---
+++ codex/elementary_programming/printrevcomb.go
@@ -0,0 +1,44 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	for i := 9; i >= 2; i-- {
+		for j := i - 1; j >= 1; j-- {
+			for k := j - 1; k >= 0; k-- {
+				if i == 2 && j == 1 && k == 0 {
+					fmt.Printf("%d%d%d\n", i, j, k)
+				} else {
+					fmt.Printf("%d%d%d, ", i, j, k)
+				}
+			}
+		}
+	}
+}
+
+
+/*
+
+func PrintRevComb() {
+	for i := '9'; i >= '2'; i-- {
+		for j := i - 1; j >= '1'; j-- {
+			for k := j - 1; k >= '0'; k-- {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+
+				// Print comma and space for all except the last one (210)
+				if !(i == '2' && j == '1' && k == '0') {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
+	*/

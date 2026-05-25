fbd27a905a7a2ab6549907ed8e9ed299f8c9a0e9
eeyung <eyungemmanuel@gmail.com>
2026-05-25 09:31:42 +0100

    understanding_elementary_programming

---
+++ codex/elementary_programming/addprimesum.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println(0)
+		return
+	}
+
+	num := os.Args[1]
+
+	if num[0] == '-' {
+		fmt.Println(0)
+		return
+	}
+
+	n := 0
+	sum := 0
+
+	for _, r := range num {
+		n = n*10 + int(r-'0')
+	}
+
+
+	for i := 2; i <= n; i++ {
+		if isPrime(i) {
+			sum += i
+		}
+	}
+
+
+	fmt.Println(sum)
+
+}
+
+func isPrime(n int)bool{
+	if n < 2 {
+		return false
+	}
+
+	if n == 2 {
+		return true
+	}
+
+	if n % 2 == 0 {
+		return false
+	}
+
+	for i := 3; i * i <= n; i += 2 {
+		if n % i == 0 {
+			return false
+		}
+	}
+
+	return true
+}

4459aa24d2b74e827eeedd1840dde43602cfbca9
eeyung <eyungemmanuel@gmail.com>
2026-05-28 13:53:46 +0100

    understanding_ascii_art_web

--- ascii-art-web-practice/project/main.go
+++ ascii-art-web-practice/project/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	// "fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,16 +15,63 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	tmpl.Execute(w, nil)
+}
+
+func asciiHandler(w http.ResponseWriter, r *http.Request) {
 	text := r.FormValue("text")
+	banner := r.FormValue("banner")
 
-	// fmt.Println(r.FormValue("text"))
-	// fmt.Println(r.FormValue("banner"))
+	tmpl, err := template.ParseFiles("templates/index.html")
 
-	tmpl.Execute(w, text)
+	if err != nil {
+		
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+
+		return
+	}
+
+	result := generateAscii(text, banner)
+
+	tmpl.Execute(w, result)
+}
+
+func generateAscii(text, banner string) string {
+	filepath := banner + ".txt"
+
+	data, err := os.ReadFile(filepath)
+
+	if err != nil {
+
+		return "Error reading banner file"
+	}
+
+	content := strings.ReplaceAll(string(data), "\r\n", "\n")
+
+
+	lines := strings.Split(content, "\n")
+
+	result := ""
+
+	for i := 0; i < 8; i++ {
+
+		for _, ch := range text {
+
+			index := int(ch-32)*9 + i
+
+			result += lines[index]
+		}
+
+		result += "\n"
+	}
+
+	return result
 }
 
 func main(){
 	http.HandleFunc("/", homeHandler)
+
+	http.HandleFunc("/ascii-art", asciiHandler)
 
 	log.Println("Server started on :8080")
 

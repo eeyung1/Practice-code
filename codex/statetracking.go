ba3454dd9d5458b5a46f1bd40cfea19936cc31fa
eeyung <eyungemmanuel@gmail.com>
2026-06-01 09:50:36 +0100

    state_tracking

--- codex/statetracking.go
+++ codex/statetracking.go
@@ -5,20 +5,57 @@ import (
 )
 
 func main() {
-	inWord := false
+	lightState := "GREEN" 
 
-	for _, ch := range "yes no ok" {
-		if ch == ' ' {
-			if !inWord {
-				fmt.Println("start of word")
-			}
+	for second := 1; second <= 4; second++ {
+		
+		fmt.Printf("[Second %d] The light is %s -> ", second, lightState)
 
-			inWord = true
-		} else {
-			inWord = false
+		switch lightState {
+		case "GREEN":
+			fmt.Println("Driver says: 'Go!'")
+			lightState = "YELLOW" 
+
+		case "YELLOW":
+			fmt.Println("Driver says: 'Slow down!'")
+			lightState = "RED"    
+
+		case "RED":
+			fmt.Println("Driver says: 'Stop!'")
+			lightState = "GREEN"  
 		}
 	}
 }
+
+// func main(){
+// 	isLightOn := true
+
+// 	for i := 1; i <= 4; i++ {
+// 		isLightOn = !isLightOn
+
+// 		if isLightOn {
+// 			fmt.Printf("click %d: The room is bright! (ON)\n", i)
+// 		} else {
+// 			fmt.Printf("Click %d: The room is dark! (OFF)\n", i)
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	inWord := false
+
+// 	for _, ch := range "yes no ok" {
+// 		if ch == ' ' {
+// 			if !inWord {
+// 				fmt.Println("start of word")
+// 			}
+
+// 			inWord = true
+// 		} else {
+// 			inWord = false
+// 		}
+// 	}
+// }
 
 // func main() {
 // 	for _, ch := range "a b" {

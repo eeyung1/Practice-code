36c5f7d554b1bf6c96037942c50faa09cfa95087
eeyung <eyungemmanuel@gmail.com>
2026-06-01 11:00:25 +0100

    understanding_ascii_art_web

--- ascii-art-web-practice/project/main.go
+++ ascii-art-web-practice/project/main.go
@@ -9,6 +9,16 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)

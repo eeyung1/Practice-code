2b2e9ab88698e454c1db9c798e4308ead631b95e
eeyung <eyungemmanuel@gmail.com>
2026-06-05 09:25:49 +0100

    understanding_elementary_programming

---
+++ codex/elementary_programming/saveandmiss.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func SaveAndMiss(arg string, num int) string {
+	if num <= 0 {
+		return arg
+	}
+
+	result := ""
+
+	save := true
+
+	for i := 0; i < len(arg); i += num {
+		end := i + num
+
+		if end > len(arg) {
+			end = len(arg)
+		}
+
+		block := arg[i:end]
+
+		if save {
+			result += block
+		}
+
+		save = !save
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(SaveAndMiss("123456789", 3))
+	fmt.Println(SaveAndMiss("abcdefghijklmnopqrstuvwyz", 3))
+	fmt.Println(SaveAndMiss("", 3))
+	fmt.Println(SaveAndMiss("hello you all ! ", 0))
+	fmt.Println(SaveAndMiss("what is your name?", 0))
+	fmt.Println(SaveAndMiss("go Exercise Save and Miss", -5))
+}

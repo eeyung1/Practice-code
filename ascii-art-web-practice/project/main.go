4ae42c4ac3c61c847b5643bf19ff33018ebdd27f
eeyung <eyungemmanuel@gmail.com>
2026-06-08 11:04:27 +0100

    understanding_ascii_art_stylize

--- ascii-art-web-practice/project/main.go
+++ ascii-art-web-practice/project/main.go
@@ -9,6 +9,9 @@ import (
 func main(){
 	http.HandleFunc("/", handlers.HomeHandler)
 
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/ascii-art", handlers.AsciiHandler)
 
 	log.Println("Server started on :8080")

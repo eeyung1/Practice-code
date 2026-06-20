76a5a1200972cd8439304a0f3f7c0f0353b93301
eeyung <eyungemmanuel@gmail.com>
2026-06-20 21:10:59 +0100

    understanding_groupie_tracker

--- groupie-tracker/services/findArtist.go
+++ groupie-tracker/services/findArtist.go
@@ -6,9 +6,11 @@ func FindArtistByID(
 	id int,
 	artists []models.Artist,
 )(*models.Artist, bool) {
-	if id < 1 || id > len(artists) {
-		return nil, false
+	for _, artist := range artists {
+		if artist.ID == id {
+			return &artist, true
+		}
 	}
 
-	return &artists[id-1], true
+	return nil, false
 }

66c633c7d754f9a76748d25e660dc926ed14c9af
eeyung <eyungemmanuel@gmail.com>
2026-07-09 11:10:57 +0100

    refactoring_the_home_handler

--- groupie-tracker-filters/handlers/home.go
+++ groupie-tracker-filters/handlers/home.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"strconv"
 
 	"groupie-tracker-filters/models"
 	"groupie-tracker-filters/services"
@@ -43,96 +42,40 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	countries := services.GetUniqueCountries(relations)
 
-	creationMin := r.URL.Query().Get("creationMin")
-	creationMax := r.URL.Query().Get("creationMax")
-
-	albumMin := r.URL.Query().Get("albumMin")
-	albumMax := r.URL.Query().Get("albumMax")
-
-	memberValues := r.URL.Query()["members"]
-
-	selectedCountries := r.URL.Query()["countries"]
-
-	var selectedMembers []int
-
-	for _, value := range memberValues {
-		memberCount, err := strconv.Atoi(value)
-		if err != nil {
-			continue
-		}
-
-		selectedMembers = append(selectedMembers, memberCount)
-	}
-
-	albumMinYear := 0
-	albumMaxYear := 9999
-
-	if albumMin != "" {
-		albumMinYear, _ = strconv.Atoi(albumMin)
-	}
-
-	if albumMax != "" {
-		albumMaxYear, _ = strconv.Atoi(albumMax)
-	}
-
-	min := 0
-	max := 9999
-
-	if creationMin != "" {
-		min, _ = strconv.Atoi(creationMin)
-	}
-
-	if creationMax != "" {
-		max, _ = strconv.Atoi(creationMax)
-	}
+	filters := services.ParseFilters(r)
 
 	artists = services.FilterByCreationDate(
 		artists,
-		min,
-		max,
+		filters.CreationMin,
+		filters.CreationMax,
 	)
 
 	artists = services.FilterByFirstAlbum(
 		artists,
-		albumMinYear,
-		albumMaxYear,
+		filters.AlbumMin,
+		filters.AlbumMax,
 	)
 
-	if len(selectedMembers) > 0 {
-		artists = services.FilterByMembers(artists, selectedMembers)
+	if len(filters.Members) > 0 {
+		artists = services.FilterByMembers(artists, filters.Members)
 	}
 
-	if len(selectedCountries) > 0 {
-		artists = services.FilterByLocation(artists, relationMap, selectedCountries)
+	if len(filters.Countries) > 0 {
+		artists = services.FilterByLocation(artists, relationMap, filters.Countries)
 	}
 
-	query := r.URL.Query().Get("search")
-	field := r.URL.Query().Get("field")
-
-	if query != "" {
+	if filters.Query != "" {
 		artists = services.SearchArtists(
-			query,
+			filters.Query,
 			artists,
 		)
 	}
 
-	order := r.URL.Query().Get("sort")
-
 	services.SortArtists(
 		artists,
-		field,
-		order,
+		filters.Field,
+		filters.Order,
 	)
-
-	// tmpl := template.Must(
-	// 	template.ParseFiles("templates/index.html"),
-	// )
-
-	// tmpl := template.Must(
-	// 	template.New("index.html").Funcs(template.FuncMap{
-	// 		"containsMember": ContainsMember,
-	// 	}).ParseFiles("templates/index.html"),
-	// )
 
 	tmpl := template.Must(
 		template.New("index.html").
@@ -146,20 +89,20 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	data := models.PageData{
 		Artists: artists,
 
-		Query: query,
-		Order: order,
-		Field: field,
+		Query: filters.Query,
+		Order: filters.Order,
+		Field: filters.Field,
 
-		CreationMin: creationMin,
-		CreationMax: creationMax,
+		CreationMin: fmt.Sprintf("%d", filters.CreationMin),
+		CreationMax: fmt.Sprintf("%d", filters.CreationMax),
 
-		AlbumMin: albumMin,
-		AlbumMax: albumMax,
+		AlbumMin: fmt.Sprintf("%d", filters.AlbumMin),
+		AlbumMax: fmt.Sprintf("%d", filters.AlbumMax),
 
-		SelectedMembers: selectedMembers,
+		SelectedMembers: filters.Members,
 
-		Countries: countries,
-		SelectedCountries: selectedCountries,
+		Countries:         countries,
+		SelectedCountries: filters.Countries,
 	}
 
 	if err := tmpl.Execute(w, data); err != nil {

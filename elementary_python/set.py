232a2b870f15f7520fad89495f89bc80b0a07ba7
eeyung <eyungemmanuel@gmail.com>
2026-07-13 09:23:00 +0100

    understanding_elementary_python

---
+++ elementary_python/set.py
@@ -0,0 +1,10 @@
+arr = [1, 2, 2, 3, 3, 3]
+
+print(set(arr))
+
+fruits = set(["avocado", "tomato", "banana"])
+vegetables = set(["beets", "carrots", "tomato"])
+
+print(fruits | vegetables)
+print(fruits & vegetables)
+print(fruits - vegetables)

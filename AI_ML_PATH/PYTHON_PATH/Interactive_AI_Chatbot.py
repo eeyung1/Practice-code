37f99558600531da083b850f79f81cf1fb27bb57
Emmanuel Eyung <eyungemmanuel@gmail.com>
2026-07-17 15:39:14 +0100

    Add interactive AI chatbot using Groq API

---
+++ AI_ML_PATH/PYTHON_PATH/Interactive_AI_Chatbot.py
@@ -0,0 +1,28 @@
+from groq import Groq
+import os
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+while True:
+    prompt = input("Ask Groq something: ")
+
+    if prompt == "exit":
+        print("Goodbye!")
+        break
+    
+    response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+    )
+
+    reply = response.choices[0].message.content
+
+
+    print(f"\nGroq says:\n{reply}")

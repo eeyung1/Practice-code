083f151c95575a1dc51daec148ec4cc27eb28331
eeyung1 <eyungemmanuel@gmail.com>
2026-07-23 14:53:42 +0100

    creating_parser

--- lem-in/main.go
+++ lem-in/main.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	err := ReadFile("example.txt")
+	rooms, err := ReadFile("example.txt")
 	if err != nil {
 		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Rooms loaded:", len(rooms))
+
+	for _, room := range rooms {
+		room.Display()
 	}
 }
--- lem-in/parser.go
+++ lem-in/parser.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func ReadFile(filename string) error {
+func ReadFile(filename string) ([]Room, error) {
 	file, err := os.Open(filename)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+
+	var rooms []Room
+
+	roomLookup := make(map[string]Room)
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -25,12 +30,12 @@ func ReadFile(filename string) error {
 		x, err := strconv.Atoi(parts[1])
 
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		y, err := strconv.Atoi(parts[2])
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		room := Room{
@@ -39,8 +44,17 @@ func ReadFile(filename string) error {
 			Y:    y,
 		}
 
-		room.Display()
+		if _, exists := roomLookup[room.Name]; exists {
+			return nil, fmt.Errorf("duplicate room name: %s", room.Name)
+		}
+
+		rooms = append(rooms, room)
+		roomLookup[room.Name] = room
 	}
 
-	return scanner.Err()
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return rooms, nil
 }

522a603db1530f9d48e86ec64b887c1a5d1d947e
Emmanuel Eyung <eyungemmanuel@gmail.com>
2026-07-24 15:45:39 +0100

    Add system_message.py for Groq chat integration

---
+++ AI_ML_PATH/PYTHON_PATH/system_message.py
@@ -0,0 +1,56 @@
+from groq import Groq
+import os
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are a world class hacker"
+    }
+]
+
+while True:
+    prompt = input("Ask Groq something: ")
+
+    if prompt.lower() == "exit":
+        print("Goodbye!")
+        break
+    
+    messages.append(
+        {
+            "role":"user",
+            "content": prompt
+        }
+    )
+    
+    response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=messages
+    )
+
+    reply = response.choices[0].message.content
+
+    prompt = response.usage.prompt_tokens
+
+    completion = response.usage.completion_tokens
+
+    total = response.usage.total_tokens
+
+    messages.append(
+        {
+
+            "role": "assistant",
+
+            "content": reply
+
+        }
+    )
+
+
+    print(f"\nGroq says:\n{reply}")
+    print(f"Prompt Tokens:\n{prompt}")
+    print(f"Completion Tokens:\n{completion}")
+    print(f"Total Tokens:\n{total}")

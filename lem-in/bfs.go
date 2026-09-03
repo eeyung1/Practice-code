849aeed452574d2ecd07581d73b266d3fe0557ef
eeyung1 <eyungemmanuel@gmail.com>
2026-09-03 14:19:53 +0100

    confirm_room_Path

--- lem-in/bfs.go
+++ lem-in/bfs.go
@@ -28,15 +28,15 @@ func BFS(graph *Graph, start string, end string) []string {
 				for currentRoom != start {
 					path = append(path, currentRoom)
 					currentRoom = parent[currentRoom]
-
-					path = append(path, start)
-
-					for i, j := 0, len(path)-1; i < j; i, j = i + 1, j - 1 {
-						path[i], path[j] = path[j], path[i]
-					}
-
-					return path
 				}
+
+				path = append(path, start)
+
+				for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+					path[i], path[j] = path[j], path[i]
+				}
+
+				return path
 			}
 		}
 	}
